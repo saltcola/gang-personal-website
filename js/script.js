a9eb5baa6ee792a0ff31719aa9c456894c732bf2
Gang Fan <gang.fan@uconn.edu>
2016-03-22 10:14:12 -0400

    Add font-mfizz, modift file structure

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 
 
 $(document).ready(function(){
+
 	$("section").hover(function(){
 		var first = $(this).attr("class").split(" ")[0];
 		$(this).removeClass();
 		$(this).addClass(first);
 		$(this).addClass("section-hover");
 	})
-
+	
 	$("section").mouseleave(function(){
 		var first = $(this).attr("class").split(" ")[0];
 		$(this).removeClass();
 		$(this).addClass(first);
 	})
-
 });
